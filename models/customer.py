d92aa41249bc98af542bd01fc0b1dc32aca118f6
alambrow <alexander.lambrow@gmail.com>
2021-07-19 10:12:12 -0500

    do get updated

--- models/customer.py
+++ models/customer.py
@@ -1,7 +1,9 @@
 class Customer():
     """Customer class def
     """
-    def __init__(self, id, name, address):
+    def __init__(self, id, name, address, email="", password=""):
         self.id = id
         self.full_name = name
         self.address = address
+        self.email = email
+        self.password = password
